4577be03c4e08f99aad30f317dcd5f4b60322958
Aye-TK <eye.thanita00@gmail.com>
2024-04-12 18:29:27 +0700

    Score

---
+++ Excercise4_Thanita_k.py
@@ -0,0 +1,11 @@
+eng = 80
+bus = 90
+coms = 99
+pro = 85
+
+print("---Your Score---")
+print("Foundation English :", eng)
+print("General Business :", bus)
+print("Introduction to Computer Systems :", coms)
+print("Computer Programming", pro)
+
