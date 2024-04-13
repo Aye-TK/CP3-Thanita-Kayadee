c10d267fb87136503c734ef9fb9d05c5f17e6937
Aye-TK <eye.thanita00@gmail.com>
2024-04-13 15:53:05 +0700

    Create lecture50

---
+++ lecture50.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(10,5)
+
+def minusNumber(x,y):
+    print(x-y)
+minusNumber(10,5)
+
+def mulNumber(x,y):
+    print(x*y)
+mulNumber(10,5)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(10,5)
